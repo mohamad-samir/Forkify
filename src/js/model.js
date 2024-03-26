82410f1778b927081b778c39f2a816d1c221ac4d
mohamad-samir <mohamad.samir2011@gmail.com>
2024-03-26 22:05:13 +0200

    config and helpers

--- src/js/model.js
+++ src/js/model.js
@@ -1,16 +1,14 @@
+import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/${id}'
-    );
-
-    const data = await res.json();
-    console.log(data, res);
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     // let recipe = data.data.recipe;
     const { recipe } = data.data;
